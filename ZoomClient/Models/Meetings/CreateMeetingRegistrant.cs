bfb6ccee93d709cf59259de5012cbc9966577209
Phil Hess <phess@andculture.com>
2018-09-03 08:30:29 -0400

    v1.0.12 Update CreateMeetingRegistrant to inherit from MeetingRegistrant base class

--- ZoomClient/Models/Meetings/CreateMeetingRegistrant.cs
+++ ZoomClient/Models/Meetings/CreateMeetingRegistrant.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace AndcultureCode.ZoomClient.Models.Meetings
 {
-    public class CreateMeetingRegistrant : Meeting, ICreatable
+    public class CreateMeetingRegistrant : MeetingRegistrant, ICreatable
     {
         #region ICreatable Implementation
 
