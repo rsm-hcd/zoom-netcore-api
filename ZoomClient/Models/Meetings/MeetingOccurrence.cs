85c4d44593eaf89c3e100554af20479e8b95c52d
Lindt, Kelly <klindt@cff.org>
2020-09-22 09:38:54 -0400

    Add additional Meeting properties, Add MeetingOccurrence, Add MeetingTrackingField

---
+++ ZoomClient/Models/Meetings/MeetingOccurrence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndcultureCode.ZoomClient.Models.Meetings
+{
+    public class MeetingOccurrence
+    {
+        /// <summary>
+        /// Zoom Property: occurrence_id
+        /// <para>Occurrence ID: Unique identifier that identifies an occurrence of a recurring webinar.
+        /// <see href="https://support.zoom.us/hc/en-us/articles/216354763-How-to-Schedule-A-Recurring-Webinar">
+        /// Recurring webinars</see> can have a maximum of 50 occurrences.</para>
+        /// </summary>
+        public string OccurenceId { get; set; }
+
+        /// <summary>
+        /// Zoom Property: start_time
+        /// </summary>
+        public DateTimeOffset? StartTime { get; set; }
+
+        /// <summary>
+        /// Zoom Property: duration
+        /// </summary>
+        public int Duration { get; set; }
+
+        /// <summary>
+        /// Zoom Property: status
+        /// </summary>
+        public string Status { get; set; }
+    }
+}
---
+++ ZoomClient/Models/Meetings/MeetingTrackingField.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AndcultureCode.ZoomClient.Models.Meetings
+{
+    public class MeetingTrackingField
+    {
+        /// <summary>
+        /// Zoom property: field
+        /// <para>Label of the tracking field.</para>
+        /// </summary>
+        public string Field { get; set; }
+
+        /// <summary>
+        /// Zoom property: value
+        /// <para>Value of the tracking field.</para>
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
